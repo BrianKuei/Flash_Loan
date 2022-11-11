d1173789a9df652fc799fa6588368d532cc10dcd
Brian Kuei <briankuei@unnotech.com>
2022-11-11 17:55:34 +0800

    chore: add comment

--- test/Compound_test.ts
+++ test/Compound_test.ts
@@ -30,9 +30,12 @@ async function deployContractsFixture() {
     comptroller = await ComptrollerFactory.deploy();
     await comptroller.deployed();
 
-    // interestModel
+    // deploy WhitePaperInterestRateModel, 將利率模型合約中的借貸利率設定為 0%
     const WhitePaperInterestRateFactory = await ethers.getContractFactory("WhitePaperInterestRateModel");
-    whitePaperInterestRate = await WhitePaperInterestRateFactory.deploy(parseUnits("0", 18), parseUnits("0", 18),);
+    whitePaperInterestRate = await WhitePaperInterestRateFactory.deploy(
+        parseUnits("0", 18),  // baseRatePerYear = 0
+        parseUnits("0", 18)   // multiplierPerYear = 0
+    );
     whitePaperInterestRate.deployed();
 
     // CErc20Immutable
@@ -64,6 +67,12 @@ async function deployContractsFixture() {
 
     comptroller._supportMarket(cErc20TokenA.address);
     comptroller._supportMarket(cErc20TokenB.address);
+
+    // 設定清算獎勵 = 108%, 清算人可以獲得清算金額的8%作為獎勵
+    await comptroller._setLiquidationIncentive(parseUnits("1.08", 18)); // 清算人獎勵
+
+    // set close factor = 50%, 一次只能清算一半的借款，保護被清算的人
+    await comptroller._setCloseFactor(parseUnits("0.5", 18)); // 可清算 token 的比例
 
     // 使用 proxy contract 要注意 initialize 會有兩個（CErc20、CToken 各有），因此要用 signature 來選 initialize method
     // CErc20["initialize(address,address,address,uint256,string,string,uint8)"](
@@ -139,7 +148,7 @@ async function setUpBorrowRepayContext() {
     await underlyingERC20TokenA.connect(userB).approve(cErc20TokenA.address, parseUnits("100", 18));
     // 會有三個 event 可以去觀察 AccrueInterest、Mint、Transfer
     const mintCTokenBTx = await cErc20TokenB.connect(userA).mint(parseUnits("1", 18));
-    const receiptMintCTokenBTxMintEvent = (await mintCTokenBTx.wait()).events?.find(e=> e.event == 'Mint');
+    const receiptMintCTokenBTxMintEvent = (await mintCTokenBTx.wait()).events?.find(e => e.event == 'Mint');
     expect(receiptMintCTokenBTxMintEvent?.args?.mintTokens).to.equal(parseUnits("1", 18));
     await cErc20TokenA.connect(userB).mint(parseUnits("100", 18));
 
@@ -148,8 +157,13 @@ async function setUpBorrowRepayContext() {
 
     // 借款
     expect(await cErc20TokenA.getCash()).to.equal(parseUnits("100", 18)); // 檢查是否有 100 顆 cTokenA
-    const borrowTx = await cErc20TokenA.connect(userA).borrow(parseUnits("50", 18));
+
+    // user1 借 50 個 TokenA，因為collateral factor 是 50%，最多也只能借50個
+    const TOKEN_A_BORROW_AMOUNT = parseUnits("50", 18);
+    const borrowTx = await cErc20TokenA.connect(userA).borrow(TOKEN_A_BORROW_AMOUNT);
+
     const receiptBorrowTxEvent = (await borrowTx.wait()).events?.find(e => e.event == 'Borrow')?.args;
+
     expect(receiptBorrowTxEvent?.borrowAmount).to.equal(parseUnits("50", 18));
     expect(await cErc20TokenA.getCash()).to.equal(parseUnits("50", 18)); // 檢查是否有 50 顆 cTokenA
 
@@ -206,24 +220,36 @@ describe("Test borrow and liquidation", function () {
             comptroller
         } = await loadFixture(setUpBorrowRepayContext);
 
-        await comptroller._setLiquidationIncentive(parseUnits("1", 18)); // 清算人的獎勵
-        await comptroller._setCloseFactor(parseUnits("0.5", 18)); // 可清算 token 的比例
+        // 設定清算獎勵 = 108%, 清算人可以獲得清算金額的8%作為獎勵
+        await comptroller._setLiquidationIncentive(parseUnits("1.08", 18)); // 清算人獎勵
+
+        // 將 Token B 的 collateral factor 降低為 20%，userA變成最多只能借20個tokenA，因此可以被清算
         await comptroller._setCollateralFactor(cErc20TokenB.address, parseUnits("0.2", 18));
         console.log('after modify collateral factor'.bgMagenta);
         await printAccountLiquidity(userA.address, comptroller);
 
-        await underlyingERC20TokenA.transfer(userB.address, parseUnits("20", 18));
-        await underlyingERC20TokenA.connect(userB).approve(cErc20TokenA.address, parseUnits("20", 18));
+        await underlyingERC20TokenA.transfer(userB.address, parseUnits("25", 18));
+        await underlyingERC20TokenA.connect(userB).approve(cErc20TokenA.address, parseUnits("25", 18));
 
-        //                              要償還的 cToken       幫償還的人               被清算的人       清算數量              想拿到的 cToken
-        const liquidateBorrowTx = await cErc20TokenA.connect(userB).liquidateBorrow(userA.address, parseUnits("20", 18), cErc20TokenB.address);
+
+        // userB 清算 userA, 清算金額 = 50 * 50%(close factor) = 25 tokenA
+        const REPAY_AMOUNT = parseUnits("25", 18);
+        //                              要償還的 cToken       幫償還的人               被清算的人       清算數量        想拿到的 cToken
+        const liquidateBorrowTx = await cErc20TokenA.connect(userB).liquidateBorrow(userA.address, REPAY_AMOUNT, cErc20TokenB.address);
+        expect(liquidateBorrowTx).to.changeTokenBalance(underlyingERC20TokenA, [cErc20TokenA, userB], [REPAY_AMOUNT, parseUnits("10", 18)])
+
         const liquidateBorrowEventInfo = (await liquidateBorrowTx.wait()).events?.reduce((map, cur) => {
             return cur?.event && !map.has(cur.event) && map.set(cur.event, cur.args), map;
         }, new Map);
         console.log('modify collateral factor result info'.bgYellow);
         console.log(liquidateBorrowEventInfo);
 
-    })
+        // 檢查 user2 獲得的 cTokenB = (清算金額 * 清算獎勵 - compound抽成2.8%)/tokenB價格
+        const user2BalanceOfCTokenB = await cErc20TokenB.balanceOf(userB.address);
+        const expectValue = (25 * 1.08 * (1 - 0.028)) / 100;
+        expect(user2BalanceOfCTokenB).to.equal(parseUnits(`${expectValue}`, 18));
+    });
+
 
     it("Use an oracle to modify the tokenB price and make userA liquidation", async () => {
         const {
@@ -236,21 +262,31 @@ describe("Test borrow and liquidation", function () {
             simplePriceOracle,
         } = await loadFixture(setUpBorrowRepayContext);
 
-        await simplePriceOracle.setUnderlyingPrice(cErc20TokenB.address, parseUnits("30", 18));
-        await comptroller._setLiquidationIncentive(parseUnits("1", 18));
-        await comptroller._setCloseFactor(parseUnits("0.5", 18));
+        // console.log(await comptroller)
+
+        // 將 tokenB 的價格設為原本的一半，變成只能借出25 tokenA，已借出50 tokenA，所以可以被清算了
+        const newTokenBPrice = parseUnits("50", 18);
+        await simplePriceOracle.setUnderlyingPrice(cErc20TokenB.address, newTokenBPrice);
+
         console.log('after modify collateral factor'.bgMagenta);
         await printAccountLiquidity(userA.address, comptroller);
 
-        await underlyingERC20TokenA.transfer(userB.address, parseUnits("20", 18));
-        await underlyingERC20TokenA.connect(userB).approve(cErc20TokenA.address, parseUnits("20", 18));
+        await underlyingERC20TokenA.transfer(userB.address, parseUnits("25", 18));
+        await underlyingERC20TokenA.connect(userB).approve(cErc20TokenA.address, parseUnits("25", 18));
 
-        // //                            要償還的 cToken.     幫償還的人               被清算的人       清算數量               想拿到的 cToken
-        const liquidateBorrowTx = await cErc20TokenA.connect(userB).liquidateBorrow(userA.address, parseUnits("20", 18), cErc20TokenB.address);
+
+        // userB 清算 userA, 清算金額 = 50 * 50%(close factor) = 25 tokenA
+        //                              要償還的 cToken.     幫償還的人               被清算的人       清算數量               想拿到的 cToken
+        const liquidateBorrowTx = await cErc20TokenA.connect(userB).liquidateBorrow(userA.address, parseUnits("25", 18), cErc20TokenB.address);
         const liquidateBorrowEventInfo = (await liquidateBorrowTx.wait()).events?.reduce((map, cur) => {
             return cur?.event && !map.has(cur.event) && map.set(cur.event, cur.args), map;
         }, new Map);
         console.log('modify tokenB price result info'.bgYellow);
         console.log(liquidateBorrowEventInfo);
+
+        // 檢查 userB 獲得的 cTokenB = (清算金額 * 清算獎勵 - compound抽成2.8%)/tokenB價格
+        const user2BalanceOfCTokenB = await cErc20TokenB.balanceOf(userB.address);
+        const expectValue = (25 * 1.08 * (1 - 0.028)) / 50;
+        expect(user2BalanceOfCTokenB).to.equal(parseUnits(`${expectValue}`, 18));
     });
 });
